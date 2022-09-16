44b4c8a555395510946731f2e5975bd36acc953d
Richard Rodriguez <108311008+rickferrodriguez@users.noreply.github.com>
2022-09-16 10:02:57 -0500

    mas tipos de datos en JavaScript class

--- JavaClass/tiposdedatos.js
+++ JavaClass/tiposdedatos.js
@@ -17,7 +17,7 @@ console.log(objeto);
 
 // Saber el tipo de elemento que llamamos para imprimir 
 // Esto lo podemos hacer con la palabra "typeof"
- console.log(typeof nombre);
+console.log(typeof nombre);
 
 
 // Tipos de datos: Boolean (true, false)
@@ -28,3 +28,26 @@ console.log(typeof booleano);
 function miFuncion(){}
 console.log(miFuncion);
 
+// Tipos de datos : Simbolo, se utiliza para crear un valor único de una variable
+var simbolo = Symbol("Mi simbolo");
+console.log(simbolo);
+
+// Tipo clase tambien va a ser una function, la diferencia es que esta en su interior si inicializa con un constructor y a su vez este tiene variables
+class Persona{
+    constructor(nombre, apellido)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+ console.log(Persona);
+
+// Tipo undefined, este viene por defecto cuando no se le define un valor a una variable
+var X;
+console.log(X); /* al imprimirlo el sistema me lo identifica como undefined */
+// tambien podemos definir el valor de undefined de la siguinete manera
+X = undefined;
+console.log(X);
+//null = ausencia de valor
+var nulo = null;
+console.log(nulo); /* este tipo de valor es un objeto que no contiene nada en su interior */
