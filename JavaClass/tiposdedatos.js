9777862f58bb652a2197a01ef391372c0e392e7a
Richard Rodriguez <108311008+rickferrodriguez@users.noreply.github.com>
2022-09-16 09:29:47 -0500

    typeof para saber el tipo de dato en JavaScript

--- JavaClass/tiposdedatos.js
+++ JavaClass/tiposdedatos.js
@@ -1,4 +1,20 @@
 // existen varios tipos de datos, el primero es de tipo String y se realiza de la siguiente manera
 var nombre = "Carlos"; /* estan van entre doble comilla */
-console.log(nombre)
+console.log(nombre);
 
+// Tipo de dato numérico
+var numero = 1000;
+console.log(numero);
+
+// Tipo de dato object
+var objeto = {
+    nombre : "Juan",
+    apellido : "Bolaños",
+    telefono : "333323"
+}
+// importante entender que todos estos datos se encuetran almacenados dentro de un objeto
+console.log(objeto);
+
+// Saber el tipo de elemento que llamamos para imprimir 
+// Esto lo podemos hacer con la palabra "typeof"
+ console.log(typeof nombre);
