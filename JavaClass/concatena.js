26b3010c3e588a3ea3ae2372eca14e3fcdd3012b
Richard Rodriguez <108311008+rickferrodriguez@users.noreply.github.com>
2022-09-27 16:37:21 -0500

    let y const

--- JavaClass/concatena.js
+++ JavaClass/concatena.js
@@ -6,3 +6,19 @@ console.log(nombreCompleto);
 
 var nummasnombre = nombre + (2 + 4);
 console.log(nummasnombre)
+
+// Declaracion de variables
+// comunmente se usa la palabra 'var' pero actualmente ya no es recomendable, es por ello que ahora se usa 'let'
+// IMPORTANTE: Destacar que no hace falta colocar una palabra reservada para crear una variable con el nombre ya es suficiente
+nombre = "Juan" /* las literales son los valores que se le colocan a las variables */
+// cabe recalcar que esto no es una buena practica
+
+// 'let' es una palabra difinida que sirve papa crear variables.
+let nombre2 = "Roberto";
+// IMPORTANTE: se pueden declarar variables y despues agregar el valor
+let inicial;
+inicial = "Juan";
+console.log(inicial);
+
+const apellidoCons = "Perez";
+// apellidoCons = "real"; en este caso el las constantes no pueden ser cambiadas nunca, si se dejara este codigo el sistema will show an error.
