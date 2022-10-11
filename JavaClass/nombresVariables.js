6a9555c7e72002ed7803a3949a684cb7dc8aeae8
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2022-10-11 16:23:04 -0500

    nombrar las variables

---
+++ JavaClass/nombresVariables.js
@@ -0,0 +1,2 @@
+// tener mucho cuidado con las mayúsculas al momento de declarar las variables
+let nombreCompleto = "Juan Perez"
--- JavaClass/tiposdedatos.js
+++ JavaClass/tiposdedatos.js
@@ -1,4 +1,4 @@
-// existen varios tipos de datos, el primero es de tipo String y se realiza de la siguiente manera
+
 var nombre = "Carlos"; /* estan van entre doble comilla */
 console.log(nombre);
 
