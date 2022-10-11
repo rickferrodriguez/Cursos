198ad1b1c8d394ea79ab574e2e6d2cd44e104d07
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2022-10-11 16:35:08 -0500

    fin parte inicial

--- JavaClass/nombresVariables.js
+++ JavaClass/nombresVariables.js
@@ -1,2 +1,10 @@
-// tener mucho cuidado con las mayúsculas al momento de declarar las variables
+// tener mucho cuidado con las mayúsculas al momento de declarar las variables, esto se debe a que si tienen el mismo nombre pero una de ellas tiene mayúsculas y la otra no, serán tomadas como variables diferentes.
+let nombrecompleto = "Ana Maria"
 let nombreCompleto = "Juan Perez"
+// las variables no pueden empezar con numeros
+
+//let 1nombreCompleto = "Mi nombre"; El sistema va a botar un mensaje de error
+
+let a1nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
