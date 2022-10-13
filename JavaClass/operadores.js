81835f9f84e42352f504cc52b39bfa44ae6e136c
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2022-10-12 22:01:20 -0500

    operadores en JavaScript

--- JavaClass/operadores.js
+++ JavaClass/operadores.js
@@ -24,5 +24,30 @@ console.log("resultado de la operación módulo (residuo): " + z)
 
 let i = 2, j = 5;
 let v = i ** j;
-console.log("resultado del exponente: " + v);
+// console.log("resultado del exponente: " + v);
 
+//
+//operadores de incremento 
+//
+//operadores de Pre-incremento (el operador ++ antes de la variable)
+v = ++i; //el Pre-incremento sirve para sumar en 1 la misma variable
+console.log("el resultado del pre-incremento es decir que se suma en 1 su valor actual es: " +v);
+
+//Post-incremento (el operador ++ después de la variable)
+let u = 0;
+
+u = j++;
+console.log("el resultado del Post-incremento, es decir, este suma su valor en 1 pero no lo hace en ese instante, si no que lo deja pendiente para cuando esta variable se la vuelva a utilizar"+ u);
+
+//operadores de Pre-decremento, funciona igual que en el ejercicio anterior pero restando en 1 su valor, exactamente en ese momento
+let k = 10;
+let t =0;
+t = --k;
+console.log("el resultado del Pre-decremento es: "+ t);
+
+//operadores de Post-decremento, es decir, este resta en uno su valor pero para cuando la variable sea reutilizada en otra ocación, es como si la dejara guardando.
+let l = 20;
+let s =0;
+s = l--;
+console.log("esta es la variable que guarda el decremento no aplicado "+ l);
+console.log("el resultado del Post-decremento es: "+ s);
