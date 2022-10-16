c24f06d071292108dbeaa15dec789681fc1e0998
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2022-10-15 21:48:16 -0500

    más operadores, más carpeta de ejercicios

---
+++ JavaClass/ejercicios/numeroPar.js
@@ -0,0 +1,8 @@
+let a = 2, b = 5;
+// recorder que '%' significa traer el reciduo
+if (b % 2 == 0){
+    console.log("este valor es par")
+}
+else {
+    console.log("este valor no es par")
+}
--- JavaClass/operadoresP2.js
+++ JavaClass/operadoresP2.js
@@ -9,3 +9,22 @@ console.log(estricto);
 
 let noEstricto = a == c; 
 console.log(noEstricto); // En este caso da true porque no es un comparador estricto y convierte el numero 3 que se encuentra como string a number
+
+//operadores distinto A en Javascript
+//La estructura es la misma solo que en este caso se coloca es signo de exclamación '!'
+
+let distinto = a != b;
+console.log("esta comparación de distinto es: " + distinto);
+
+
+let disEstricto = a !== c;
+console.log("Este distinto estricto es: " + disEstricto); // Al igual que en el ejercicio anterior Javascript tiene un oporador de distinto estricto, es decir, que compara si el tipo de valor es el mismo
+
+// Operadores relacionales
+
+let relacion = a < b;
+console.log("Esta relacion de menor que es: " + relacion); // tambien se puede hacer con el mayor y el igual
+
+let relacionMenIgu = a <= b; // Menor o igual
+console.log("Esta relacion menor o igual es: " + relacionMenIgu); // tambien se puede con el mayor igual
+
