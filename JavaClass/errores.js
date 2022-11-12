0224abc46fa0d27f598d8f4b7e121869ba203855
Richard rodriguez <richardfernandorodriguez@gmail.com>
2022-11-12 15:15:24 -0500

    clausula throws en JavaScript para errores personalizados

--- JavaClass/errores.js
+++ JavaClass/errores.js
@@ -12,8 +12,24 @@ finally{
     console.log('terminamos con la revisión de errores')
 }
 // el método 'finally' se ejecuta siempre, sin importar si el error se presenta o no, pero este es opcional
-
 // Cuando ocurre un error que no es tratado en el sistema el código deja de ejecutarse, pero por ejemplo al usar catch podemos seguir ejecutando los metodos incluso cuando se presenta un error
 
-console.log('continuamos...')
 
+// Cláusula 'THROWS'
+// sirve para lanzar nuestros propios errores
+
+let resultado = '';
+
+try {
+    // en este caso el mensaje creado es este
+    if(isNaN(resultado)) throw 'No es un número'
+    else if(resultado === '') throw 'Es una cadena vacia'
+}
+catch (error){
+    console.log(error);
+    // console.log(error.name);
+    // console.log(error.message);
+}
+finally{
+    console.log('finally method executing')
+}
