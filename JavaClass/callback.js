dca54a03e096bdcf70f4f594e95610d0e2860a3b
Richard rodriguez <richardfernandorodriguez@gmail.com>
2022-11-14 21:05:39 -0500

    revisando de nuevo los callbacks y el settimeout para ejecutar algo después de un tiempo determinado

--- JavaClass/callback.js
+++ JavaClass/callback.js
@@ -16,3 +16,24 @@ function sumar (op1, op2, funcionCallback){
 
 // al momento de utilizar nuestra función,con solo nombrarla el sistema ya comprende que estamos llamando a otra función
 sumar(4,2,imprimir)
+
+// Función 'set timeout' o llamadas asíncronas
+
+function miFuncionCallback(){
+    console.log('Pero mañana te toca pierna')
+}
+
+// setTimeout(miFuncionCallback, 3000 ); // después de tres segundos
+
+const ejercicio = (elEjercicio) => {console.log(elEjercicio)}
+
+function gimnasio (dia, funcionCallback){
+    if(dia == 'lunes'){
+        funcionCallback(`El día de hoy ${dia} te toca: Pecho`);
+        setTimeout(miFuncionCallback, 3000); 
+    }
+    else 
+        return console.log('Este día no se encuentra registrado')
+}
+
+gimnasio('martes',ejercicio)
