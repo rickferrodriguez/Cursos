1ea6c24640ce20bef212bf7a503958262e16ccce
Richard rodriguez <richardfernandorodriguez@gmail.com>
2022-11-16 22:06:59 -0500

    promises dentro de JavaScript

---
+++ JavaClass/promesas.js
@@ -0,0 +1,23 @@
+let miPromesa =new Promise((resolve, reject) =>{
+    let laPromesa = true;
+    if (laPromesa == true)
+        resolve("Resuelto completado");
+    else
+        reject("Se produjo un error");
+});
+
+// el 'then' sirve para llamar a la promesa creada, lo que se encuentra dentro de los parentecis es como si fueran los parametros de la promesa
+// miPromesa.then(valor => console.log(valor),error => console.log(error));
+
+// En este caso no se utiliza el caso de rechazo como un parametro de la promesa, si no como un aspecto por fuera de ella pero el resultado es el mismo
+miPromesa.then(valor => console.log(valor)).catch(error => console.log(error));
+
+let miEjercicio = new Promise((resolve, error) =>{
+    let elEjercicio = 'Espalda';
+    if (elEjercicio == 'Espalda')
+        resolve("El día de hoy es miercoles");
+    else 
+        error("Este día no está registrado");
+})
+
+miEjercicio.then(valor => console.log(valor)).catch(error => console.log(error));
