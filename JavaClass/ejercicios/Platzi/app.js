710b2fb2ae5e44ea4d48d6861d5d73b1f3d87d74
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-19 14:40:44 -0500

    remembering about the event listener

--- JavaClass/ejercicios/Platzi/app.js
+++ JavaClass/ejercicios/Platzi/app.js
@@ -1,12 +1,26 @@
-const $titulo = document.querySelector('h1')
-const $button = document.querySelector('button')
+// const $btn_cal = document.querySelector("#btn-calcular")
+const $result = document.querySelector("#result")
+// se agrega un id al formulario para escuchar su evento
+const $form = document.querySelector("#form")
+const $cal1 = document.querySelector("#calculo1")
+const $cal2 = document.querySelector("#calculo2")
 
-$button.addEventListener('click', () =>{
-    let texto = 'cambio de titulo on click'
-    // el toggle parece que permite cambiar entre un valor u otro
-    $titulo.innerText = texto
+
+// el evento de submit llama al último botón del formulario
+// el evento se agrega de forma automática debido a que es un formulario
+$form.addEventListener('submit', (event) =>{
+    // esta función evita que se recarge la página
+    event.preventDefault()
+    let suma = parseInt($cal1.value) + parseInt($cal2.value)
+    $result.innerHTML = suma
 })
 
-const img = document.createElement('img')
-img.setAttribute('src', 'https://static.platzi.com/media/platzi-isotipo@2x.png')
-$titulo.appendChild(img)
+$result.addEventListener('onclick', () => {
+    $result.classList.toggle('red-hover')
+})
+// $btn_cal.addEventListener("click", () => {
+//     const $cal1 = parseInt(document.querySelector("#calculo1").value)
+//     const $cal2 = parseInt(document.querySelector("#calculo2").value)
+//     let suma = $cal1 + $cal2
+//     $result.innerHTML = suma
+// });
