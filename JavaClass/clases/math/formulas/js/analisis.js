77833bacb1fc72e00fbb32fb9429bd9f8280b92b
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-01 13:40:07 -0500

    creating again the calcularMediana Function

--- JavaClass/clases/math/formulas/js/analisis.js
+++ JavaClass/clases/math/formulas/js/analisis.js
@@ -22,4 +22,27 @@ const arraySalarios = (persona) => {
 
 const arrayOrden = arraySalarios("Juanita").sort( (a,b) => a - b )
 const arrayPar = () => !(arrayOrden.length % 2)
+
+const calcularMediana = () =>{
+    let iGeneral = 0
+    let text = ''
+    if(arrayPar()){
+        iGeneral = Math.round(arrayOrden.length / 2)
+        console.log(iGeneral)
+    } else {
+        let inImpar = Math.round(arrayOrden.length / 2)
+        text = `el array es impar, la mediana es ${arrayOrden[inImpar]}`
+        console.log(text)
+    }
+
+    if(iGeneral > 0){
+        let iActual = iGeneral - 1
+        let iSiguiente = iGeneral
+        let sum = parseInt(arrayOrden[iActual])+ parseInt(arrayOrden[iSiguiente])
+        let divi = sum / 2
+        text = `el array es par, la mediana es ${divi}`
+        console.log(text)
+    }
+}
 console.log(arrayPar())
+calcularMediana()
