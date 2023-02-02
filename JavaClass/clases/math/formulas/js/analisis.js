d9717a9469187f64ce0c831f995f737f5ec27924
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-02 14:21:59 -0500

    listing all the salary of the array

--- JavaClass/clases/math/formulas/js/analisis.js
+++ JavaClass/clases/math/formulas/js/analisis.js
@@ -4,7 +4,6 @@ const $ulSalary = document.querySelector('#ul-salary')
 const $ulPorc = document.querySelector('#ul-salary')
 
 
-
 const encontrarPersona = (persona) => {
     return salarios.find(sal => sal.name === persona)
 }
@@ -20,7 +19,6 @@ const arraySalarios = (persona) => {
     const salarios = arrayTrabajos(persona).map(trab =>{
         return trab.salario
     })
-    console.log(salarios)
     return salarios
 }
 
@@ -31,11 +29,9 @@ const calcularMediana = (arr, par) =>{
     let text = ''
     if(par){
         iGeneral = Math.round(arr.length / 2)
-        console.log(iGeneral)
     } else {
         let inImpar = Math.round(arr.length / 2)
         text = `el array es impar, la mediana es ${arr[inImpar]}`
-        console.log(text)
     }
 
     if(iGeneral > 0){
@@ -44,8 +40,8 @@ const calcularMediana = (arr, par) =>{
         let sum = parseInt(arr[iActual])+ parseInt(arr[iSiguiente])
         let divi = sum / 2
         text = `el array es par, la mediana es ${divi.toFixed(2)}`
-        console.log(text)
     }
+    return text
 }
 
 
@@ -64,15 +60,22 @@ const porcSalary = (persona) => {
     })
 }
 
+const mostrarSalarios = (arr) => {
+    let texto = ``
+    arr.forEach(a => {
+        texto += `<li>${a}</li>`
+    })
+    $ulSalary.innerHTML = texto
+}
+
 $mostrar.addEventListener('click', () => {
     let valor = $inpPersona.value
     arrayOrden = arraySalarios(valor).sort( (a,b) => a - b )
+    mostrarSalarios(arrayOrden)
     let arrayPar = () => !(arrayOrden.length % 2)
     calcularMediana(arrayOrden, arrayPar())
     porcSalary(valor)
     let porOrden = arrPorc.sort( (a,b) => a - b )
     let arrayParPor= () => !(porOrden.length % 2)
-    console.log(porOrden)
-    console.log(arrayParPor())
     calcularMediana(porOrden, arrayParPor())
 })
