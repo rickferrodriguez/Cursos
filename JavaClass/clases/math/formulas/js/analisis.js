3cdbcfb9983570b00b0d74fb80b8610e7ab4760e
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-02 11:48:53 -0500

    changing the function for different values

--- JavaClass/clases/math/formulas/js/analisis.js
+++ JavaClass/clases/math/formulas/js/analisis.js
@@ -1,4 +1,9 @@
-console.log(salarios)
+const $inpPersona = document.querySelector('#inp-persona')
+const $mostrar = document.querySelector('#mostrar')
+const $ulSalary = document.querySelector('#ul-salary')
+const $ulPorc = document.querySelector('#ul-salary')
+
+
 
 const encontrarPersona = (persona) => {
     return salarios.find(sal => sal.name === persona)
@@ -19,48 +24,55 @@ const arraySalarios = (persona) => {
     return salarios
 }
 
+let arrayOrden = []
 
-const arrayOrden = arraySalarios("Juanita").sort( (a,b) => a - b )
-const arrayPar = () => !(arrayOrden.length % 2)
-
-const calcularMediana = () =>{
+const calcularMediana = (arr, par) =>{
     let iGeneral = 0
     let text = ''
-    if(arrayPar()){
-        iGeneral = Math.round(arrayOrden.length / 2)
+    if(par){
+        iGeneral = Math.round(arr.length / 2)
         console.log(iGeneral)
     } else {
-        let inImpar = Math.round(arrayOrden.length / 2)
-        text = `el array es impar, la mediana es ${arrayOrden[inImpar]}`
+        let inImpar = Math.round(arr.length / 2)
+        text = `el array es impar, la mediana es ${arr[inImpar]}`
         console.log(text)
     }
 
     if(iGeneral > 0){
         let iActual = iGeneral - 1
         let iSiguiente = iGeneral
-        let sum = parseInt(arrayOrden[iActual])+ parseInt(arrayOrden[iSiguiente])
+        let sum = parseInt(arr[iActual])+ parseInt(arr[iSiguiente])
         let divi = sum / 2
-        text = `el array es par, la mediana es ${divi}`
+        text = `el array es par, la mediana es ${divi.toFixed(2)}`
         console.log(text)
     }
 }
-calcularMediana()
 
 
-const arrPorc = []
-arraySalarios('Juanita').forEach((_, index, arr) => {
-    let res = (arr[index]) - (arr[index- 1])
-    let div = res / (arr[index -1])
-    if(isNaN(div)) {
-        return
-    }
-    else {
-        arrPorc.push(div)
-        return
-    }
+let arrPorc = []
+const porcSalary = (persona) => {
+    arraySalarios(persona).forEach((_, index, arr) => {
+        let res = (arr[index]) - (arr[index- 1])
+        let div = res / (arr[index -1])
+        if(isNaN(div)) {
+            return
+        }
+        else {
+            arrPorc.push(div)
+            return
+        }
+    })
+}
+
+$mostrar.addEventListener('click', () => {
+    let valor = $inpPersona.value
+    arrayOrden = arraySalarios(valor).sort( (a,b) => a - b )
+    let arrayPar = () => !(arrayOrden.length % 2)
+    calcularMediana(arrayOrden, arrayPar())
+    porcSalary(valor)
+    let porOrden = arrPorc.sort( (a,b) => a - b )
+    let arrayParPor= () => !(porOrden.length % 2)
+    console.log(porOrden)
+    console.log(arrayParPor())
+    calcularMediana(porOrden, arrayParPor())
 })
-
-console.log(arrPorc)
-const porOrden = arrPorc.sort( (a,b) => a - b )
-console.log(porOrden)
-
