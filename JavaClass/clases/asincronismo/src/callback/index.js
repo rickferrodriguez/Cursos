8c6e782935d43cc8a1b6cb6b4932ea8e562cabfc
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-10 22:13:00 -0500

    callback midudev

--- JavaClass/clases/asincronismo/src/callback/index.js
+++ JavaClass/clases/asincronismo/src/callback/index.js
@@ -8,8 +8,32 @@ const calc = (a,b, callback) => {
 
 console.log(calc(2,2, suma))
 
-setTimeout(() => {
-    console.log('Hola Javascript')
-}, 2000);
+// setTimeout(() => {
+//     console.log('Hola Javascript')
+// }, 2000);
+
+const saludo = (name) => {
+    console.log(`Hola ${name}`)
+}
+
+// setTimeout(() => {
+//     saludo('richard')
+// }, 3000);
+
+const modify = (arr, callback) => {
+    // hacemos algo
+    arr.push('Tatiana')
+
+    // después de hacer algo
+    callback(arr)
+}
+const lista = ['richard', 'sofia', 'magda']
+
+modify(lista, function (array) {
+    console.log(`He modificado el array y ahora tiene ${array.length}`)
+})
+
+
+
 
 
