7df5c2605e3ba839b7c77d15ad8b0d876a67aadd
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-11 18:19:41 -0500

    callback de prueba

--- JavaClass/clases/asincronismo/src/callback/index.js
+++ JavaClass/clases/asincronismo/src/callback/index.js
@@ -33,6 +33,16 @@ modify(lista, function (array) {
     console.log(`He modificado el array y ahora tiene ${array.length}`)
 })
 
+const ultCallback = (num1, num2) => {
+    console.log(num1 + num2)
+    return num1 + num2
+}
+
+const callback = (a,b,callback) => {
+    return callback(a,b)
+}
+
+callback(2,3,ultCallback)
 
 
 
