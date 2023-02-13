051c795c82495748ee1c1670fc4e427f39dc3315
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-12 22:50:41 -0500

    another example for the promises

--- JavaClass/clases/asincronismo/src/promise/index.js
+++ JavaClass/clases/asincronismo/src/promise/index.js
@@ -89,3 +89,23 @@ const operation = (num1, num2) => {
 }
 
 operation(5,3).then(total => console.log(total))
+
+function showCircle(cx, cy, radius) {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    document.body.append(div);
+
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+
+      div.addEventListener('transitionend', function handler() {
+        div.removeEventListener('transitionend', handler);
+        callback(div);
+      });
+    }, 0);
+  }
