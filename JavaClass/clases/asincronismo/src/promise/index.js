9d1b277fb051376e6111ffd61a56e296308bc084
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-13 10:11:30 -0500

    remove the radius promise function

--- JavaClass/clases/asincronismo/src/promise/index.js
+++ JavaClass/clases/asincronismo/src/promise/index.js
@@ -81,7 +81,7 @@ delay(3000).then(
 
 const operation = (num1, num2) => {
     const result = num1 + num2
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
             resolve(result)
         }, 500);
@@ -90,22 +90,4 @@ const operation = (num1, num2) => {
 
 operation(5,3).then(total => console.log(total))
 
-function showCircle(cx, cy, radius) {
-    let div = document.createElement('div');
-    div.style.width = 0;
-    div.style.height = 0;
-    div.style.left = cx + 'px';
-    div.style.top = cy + 'px';
-    div.className = 'circle';
-    document.body.append(div);
 
-    setTimeout(() => {
-      div.style.width = radius * 2 + 'px';
-      div.style.height = radius * 2 + 'px';
-
-      div.addEventListener('transitionend', function handler() {
-        div.removeEventListener('transitionend', handler);
-        callback(div);
-      });
-    }, 0);
-  }
