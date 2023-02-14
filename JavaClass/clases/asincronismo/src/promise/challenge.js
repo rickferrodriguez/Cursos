1b8c8305317261dc1d3f1d2724350f236f6570cc
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-14 16:42:48 -0500

    fetch con promises

---
+++ JavaClass/clases/asincronismo/src/promise/challenge.js
@@ -0,0 +1,32 @@
+// import fetch from 'node-fetch'
+
+const API = 'https://api.escuelajs.co/api/v1'
+
+const fetchData = (urlApi) => {
+    return fetch(urlApi)
+}
+
+// fetchData(`${API}/products`)
+//     .then(res => res.json())
+//     .then(products => {
+//         console.log(products)
+//     })
+//     .catch(error => console.log(error))
+
+fetchData(`${API}/products`)
+    .then(res => res.json())
+    .then(products => {
+        console.log(products[0])
+        return fetchData(`${API}/products/${products[0].id}`)
+    })
+    .then(response => response.json())
+    .then(product => {
+        console.log(product.title)
+        return fetchData(`${API}/categories/${product.category.id}`)
+    })
+    .then(response => response.json())
+    .then(category => {
+        console.log(category.name)
+    })
+    .catch(err => console.log(err))
+    .finally( () => console.log('Finally'))
