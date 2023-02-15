7c60554c676fe00e002663f6d60d20f0fae041fe
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-15 14:25:07 -0500

    listando el nombre de los productos electronicos

--- JavaClass/clases/asincronismo/src/promise/challenge.js
+++ JavaClass/clases/asincronismo/src/promise/challenge.js
@@ -1,4 +1,4 @@
-// import fetch from 'node-fetch'
+const $listado = document.querySelector('#listado')
 
 const url = 'https://api.escuelajs.co/api/v1'
 
@@ -16,17 +16,23 @@ const fetchData = (urlApi) => {
 fetchData(`${url}/products`)
     .then(res => res.json())
     .then(products => {
-        console.log(products[0])
-        return fetchData(`${url}/products/${products[0].id}`)
+        let texto = ''
+        const filtrado = products.filter(product => product.category.name.startsWith('El'))
+        filtrado.forEach(product => {
+            texto += `<li>${product.title}</li>`
+        })
+        $listado.innerHTML = texto
+        console.log(filtrado)
+        // return fetchData(`${url}/products/${products[0].id}`)
     })
-    .then(response => response.json())
-    .then(product => {
-        console.log(product.title)
-        return fetchData(`${url}/categories/${product.category.id}`)
-    })
-    .then(response => response.json())
-    .then(category => {
-        console.log(category.name)
-    })
-    .catch(err => console.log(err))
-    .finally( () => console.log('Finally'))
+    // .then(response => response.json())
+    // .then(product => {
+    //     console.log(product.title)
+    //     return fetchData(`${url}/categories/${product.category.id}`)
+    // })
+    // .then(response => response.json())
+    // .then(category => {
+    //     console.log(category.name)
+    // })
+    // .catch(err => console.log(err))
+    // .finally( () => console.log('Finally'))
