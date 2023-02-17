754d57955040f770260de640aefd120b8e65729a
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-16 22:24:31 -0500

    adding some styles for the card

--- JavaClass/clases/asincronismo/src/async/challenge.js
+++ JavaClass/clases/asincronismo/src/async/challenge.js
@@ -1,4 +1,5 @@
 const API = 'https://api.escuelajs.co/api/v1'
+const $picture_product = document.querySelector('#picture-product')
 
 async function fetchData(url) {
     const response = await fetch(url)
@@ -9,12 +10,13 @@ async function fetchData(url) {
 const anotherFunction = async (urlApi) => {
     try {
         const products = await fetchData(`${urlApi}/products`)
-        const product = await fetchData(`${urlApi}/products/${products[0].id}`)
+        const product = await fetchData(`${urlApi}/products/${products[12].id}`)
         const category = await fetchData(`${urlApi}/categories/${product.category.id}`)
 
         console.log(products[0])
         console.log(product.title)
         console.log(product.images)
+        $picture_product.innerHTML = `<img class="img-card" src="${product.images[0]}">`
         console.log(category.name)
     } catch (error) {
         console.error(error)
