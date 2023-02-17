4b7a33ac41219180782ed7309f3097c0f23eb10b
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-17 14:07:16 -0500

    getting all products

--- JavaClass/clases/asincronismo/src/async/challenge.js
+++ JavaClass/clases/asincronismo/src/async/challenge.js
@@ -1,9 +1,10 @@
-const API = 'https://api.escuelajs.co/api/v1'
+const API = 'https://dummyjson.com/products'
 const $image_card = document.querySelector('#image-card')
 const $prod_category = document.querySelector('#prod-category')
 const $prod_title = document.querySelector('#prod-title')
 const $prod_description = document.querySelector('#prod-description')
 const $prod_price = document.querySelector('#prod-price')
+const $main = document.querySelector('#main')
 
 async function fetchData(url) {
     const response = await fetch(url)
@@ -21,22 +22,48 @@ const formatoNumero = async (num) => {
 
 const anotherFunction = async (urlApi) => {
     try {
-        const products = await fetchData(`${urlApi}/products`)
-        const product = await fetchData(`${urlApi}/products/${products[12].id}`)
-        const category = await fetchData(`${urlApi}/categories/${product.category.id}`)
-        const precioFormato = await formatoNumero(product.price)
+        const products = await fetchData(urlApi)
+        // const product = await fetchData(`${urlApi}/products/${products[12].id}`)
+        // const category = await fetchData(`${urlApi}/categories/${product.category.id}`)
+        const allProd = products.products
 
-        console.log(products[12])
-        console.log(product.title)
-        console.log(product.images)
-        $image_card.innerHTML = `<img class="img-card" src="${product.images[0]}">`
-        $prod_category.textContent = category.name
-        $prod_title.textContent = product.title
-        $prod_description.textContent = product.description
-        $prod_price.textContent = precioFormato
+        let texto = ''
+        allProd.forEach((prod, index) => {
+            if(index > 12) {
+                texto += `
+            <article class="product">
+                    <picture id="image-card">
+                        <img src="${prod.images[0]}" alt="">
+                    </picture>
+
+                    <div class="prod-desc">
+                        <p id="prod-category" class="prod-category">${prod.category}</p>
+                        <h1 id="prod-title" class="prod-title">${prod.title}</h1>
+                        <p id="prod-description">${prod.description}</p>
+                        <p id="prod-price" class="prod-price">300</p>
+                        <!-- <button class="fav-btn">Favoritos</button> -->
+                        <button class="add-btn">Añadir al Carrito</button>
+                    </div>
+            </article>
+`
+            }
+            $main.innerHTML = texto
+        });
+
+        // const precioFormato = await formatoNumero(prod12.price)
+        // console.log(products)
+        // console.log(prod12.title)
+        // console.log(prod12.images)
+        // $image_card.innerHTML = `<img class="img-card" src="${prod12.images[0]}">`
+        // $prod_category.textContent = prod12.category
+        // $prod_title.textContent = prod12.title
+        // $prod_description.textContent = prod12.description
+        // $prod_price.textContent = precioFormato
     } catch (error) {
         console.error(error)
     }
 }
+
+
 
 anotherFunction(API)
