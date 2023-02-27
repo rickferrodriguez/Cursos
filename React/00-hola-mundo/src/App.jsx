9a64b3ef82b0890f219019530348ca15f80015c8
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-27 11:22:06 -0500

    pasando funciones como props, clase de react

--- React/00-hola-mundo/src/App.jsx
+++ React/00-hola-mundo/src/App.jsx
@@ -1,7 +1,24 @@
 import { TwitterCard } from './twitterCard.jsx'
 
 export function App () {
+    const format = (userName) => `@${userName}`
     return (
-        <TwitterCard/>
+        <section className="App">
+            <TwitterCard 
+                formatUserName={format} 
+                isFollowing 
+                name="Miguel Ángel Durán" 
+                userName="midudev"/>
+            <TwitterCard 
+                formatUserName={format} 
+                isFollowing={false} 
+                name="Pablo Hernandez" 
+                userName="pheralb"/>
+            <TwitterCard 
+                formatUserName={format} 
+                isFollowing 
+                name="Elon Musk" 
+                userName="elonmusk"/>
+        </ section>
     )
 }
--- React/00-hola-mundo/src/twitterCard.jsx
+++ React/00-hola-mundo/src/twitterCard.jsx
@@ -1,12 +1,26 @@
-export function TwitterCard (){
+import './App.css'
+export function TwitterCard ({formatUserName, userName, name, isFollowing}){
+    const imageSrc = `https://unavatar.io/${userName}`
     return (
-        <article>
-            <header className='tw-followCard'>
+        <article className="tw-followCard">
+            <header className='tw-followCard-header'>
                 <img 
                     className='tw-followCard-avatar'
-                    alt='El avatar de Rick'
-                    src='https://unavatar.io/midudev' />
+                    alt='El avatar de midudev'
+                    src={imageSrc} />
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span
+                        className='tw-followCard-infoUserName'>{formatUserName(userName)}
+                    </span>
+                </div>
             </header>
+
+            <aside>
+                <button
+                    className='tw-followCard-button'
+                    type="button">Seguir</button>
+            </aside>
         </article>
     )
 }
