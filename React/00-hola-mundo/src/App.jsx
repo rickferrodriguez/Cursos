32575863c08f43cd52625ee97993098668f00315
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-02-27 11:48:59 -0500

    children dentro de los componentes

--- React/00-hola-mundo/src/App.jsx
+++ React/00-hola-mundo/src/App.jsx
@@ -1,24 +1,25 @@
 import { TwitterCard } from './twitterCard.jsx'
 
 export function App () {
-    const format = (userName) => `@${userName}`
-    return (
-        <section className="App">
-            <TwitterCard 
-                formatUserName={format} 
-                isFollowing 
-                name="Miguel Ángel Durán" 
-                userName="midudev"/>
-            <TwitterCard 
-                formatUserName={format} 
-                isFollowing={false} 
-                name="Pablo Hernandez" 
-                userName="pheralb"/>
-            <TwitterCard 
-                formatUserName={format} 
-                isFollowing 
-                name="Elon Musk" 
-                userName="elonmusk"/>
-        </ section>
-    )
+  // const format = (userName) => `@${userName}`
+  return (
+    <section className="App">
+      <TwitterCard 
+        // formatUserName={} 
+        isFollowing 
+        name="Miguel Ángel Durán" 
+        userName="midudev">
+        Miguel Ángel Durán
+      </TwitterCard>
+
+      <TwitterCard 
+        // formatUserName={format} 
+        isFollowing={false} 
+        name="Pablo Hernandez" 
+        userName="pheralb">
+        Pablo Hernandez
+      </TwitterCard>
+
+    </section>
+  )
 }
--- React/00-hola-mundo/src/twitterCard.jsx
+++ React/00-hola-mundo/src/twitterCard.jsx
@@ -1,26 +1,28 @@
 import './App.css'
-export function TwitterCard ({formatUserName, userName, name, isFollowing}){
-    const imageSrc = `https://unavatar.io/${userName}`
-    return (
-        <article className="tw-followCard">
-            <header className='tw-followCard-header'>
-                <img 
-                    className='tw-followCard-avatar'
-                    alt='El avatar de midudev'
-                    src={imageSrc} />
-                <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
-                    <span
-                        className='tw-followCard-infoUserName'>{formatUserName(userName)}
-                    </span>
-                </div>
-            </header>
+export function TwitterCard ({ children,userName, isFollowing}){
+  const imageSrc = `https://unavatar.io/${userName}`
+  return (
+    <article className="tw-followCard">
+      <header className='tw-followCard-header'>
+        <img 
+          className='tw-followCard-avatar'
+          alt='El avatar de midudev'
+          src={imageSrc} />
+        <div className='tw-followCard-info'>
+          <strong>{children}</strong>
+          <span
+            className='tw-followCard-infoUserName'>{`@${userName}`}
+          </span>
+        </div>
+      </header>
 
-            <aside>
-                <button
-                    className='tw-followCard-button'
-                    type="button">Seguir</button>
-            </aside>
-        </article>
-    )
+      <aside>
+        <button
+          className='tw-followCard-button'
+          type="button">
+          Seguir
+        </button>
+      </aside>
+    </article>
+  )
 }
