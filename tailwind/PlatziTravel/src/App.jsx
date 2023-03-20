ec610f19584dd9eee7de7072abdb56da93bf8605
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-19 23:55:12 -0500

    youtube tailwind class

--- tailwind/PlatziTravel/src/App.jsx
+++ tailwind/PlatziTravel/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
 
+const Title = ({ children }) => {
+  return (
+    <h2 className='text-gray-200'>{children}</h2>
+  )
+}
+
 function App () {
   return (
-    <main className='flex item-center'>
-      <h1 className='w-64 bg-red-900'>hola</h1>
+    <main className='flex items-center justify-center h-screen bg-slate-800'>
+      <section>
+        <button className='w-52 border-2 border-sky-800 bg-sky-500 rounded p-4 text-white
+          hover:bg-sky-600 hover:text-gray-300'
+        >
+          Save Changes
+        </button>
+        <Title>hola</Title>
+        <Title>hola</Title>
+      </section>
     </main>
   )
 }
