f351e2af0d9f4c23bea89be6efd992998aef60a1
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-03-19 22:49:37 -0500

    PlatziTravel

---
+++ tailwind/PlatziTravel/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App () {
+  return (
+    <main className='flex item-center'>
+      <h1 className='w-64 bg-red-900'>hola</h1>
+    </main>
+  )
+}
+
+export default App
