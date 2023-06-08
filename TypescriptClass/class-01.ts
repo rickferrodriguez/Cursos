4427cf5fe9d20309286a1930081de0dfa73b4b24
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-06-08 11:32:27 -0500

    tipos de datos clase typescript

--- TypescriptClass/class-01.ts
+++ TypescriptClass/class-01.ts
@@ -1,1 +1,27 @@
-const firstName : string = 'Richard'
+let firstName : string = 'Richard'
+
+// firstName = 2 no es posible realizar esta transformación porque es de tipo string y queremos asingnar un número
+
+// cuando se crean objetos sencillos typescript es capaz de leer los datos que contiene y hacer inferencia de su tipo
+const persona = {
+  name: 'Pepe', //infiere que es de tipo string
+  age: 30 // infiere que es de tipo number
+}
+
+persona.age // typescript es capaz de traerme autocompletado para los objetos, agiliza el desarrollo
+
+// tambien infiere el tipo en variables sencillas
+const number = 1
+let n: number = 2
+
+let a = 'hola'
+let b: string = 'hola 2'
+
+let myAny // por defecto typescript infiere que una variable que no sabe lo que tiene es de tipo any => que ignora cualquier tipo
+
+let myUnknown: unknown = 'hola' // este tipo de
+
+
+function suma ( a: number, b: number) {
+  return a + b
+}
