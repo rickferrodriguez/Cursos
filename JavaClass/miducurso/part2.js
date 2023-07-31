333f71f5c5519547aa1924c7c710ea75d320b25e
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-07-31 10:58:32 -0500

    basic conditional

---
+++ JavaClass/miducurso/part2.js
@@ -0,0 +1,10 @@
+// the conditional code only works when the code above its import { connect } from 'react-redux'
+
+const edad = 18
+
+if ( edad > 18) {
+  console.log('Es mayor de edad')
+} else {
+  //this code only works when the above is false
+  console.log('Es menor de edad')
+}
