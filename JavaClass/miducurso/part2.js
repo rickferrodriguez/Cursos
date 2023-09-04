e846ce430901b4b9bdcfda1368aff36387dc3df2
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-09-04 14:18:12 -0500

    while examples

--- JavaClass/miducurso/part2.js
+++ JavaClass/miducurso/part2.js
@@ -2,9 +2,40 @@
 
 const edad = 18
 
-if ( edad > 18) {
+if (edad > 18) {
   console.log('Es mayor de edad')
 } else {
   //this code only works when the above is false
   console.log('Es menor de edad')
+}
+
+// while bucle , continue and break
+
+let count = 10
+
+while (count >= 0) {
+  count = count - 1
+  console.log(count + 'segundos')
+}
+
+// break
+
+while (count >= 0) {
+  count = count - 1
+  if (count === 5) {
+    break // break stops the cicle or the while bucle
+  }
+
+  console.log(count + 'segundos')
+}
+
+// continue
+
+while (count >= 0) {
+  count = count - 1
+  if (count % 2 == 0) {
+    continue // allows to continue or aboid the code below and print the things there are not in the condition
+  }
+
+  console.log(count + 'segundos')
 }
